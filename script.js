f3035348cd9f667ae93fe2646ad5583b7340f13a
Spinelsky <riyaahujaa07@gmail.com>
2025-03-15 15:38:10 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Basic Form Handling
+document.querySelector('form').addEventListener('submit', function (e) {
+  e.preventDefault();
+  alert('Thank you for reaching out! We will get back to you soon.');
+});
